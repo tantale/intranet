1c2d931b536b0cd320031b4f3ef5f2872fbff737
Laurent LAPORTE <sandlol2009@gmail.com>
2013-09-21 09:15:40 +0200

    MOD: update the list of colors and add a missing entry in 'OrderCat'.

--- Intranet-1.0/intranet/websetup/bootstrap.py
+++ Intranet-1.0/intranet/websetup/bootstrap.py
@@ -16,38 +16,41 @@ def bootstrap(command, conf, vars):  # @ReservedAssignment
     try:
         cat_dict = collections.OrderedDict()
         cat_dict.update({u"Commandes client":
-                         [dict(cat_name=u"colorCuisines",
-                               label=u"Cuisines",
-                               css_def=u"background-color: yellow; color: black;"),
-                          dict(cat_name=u"colorBains",
-                               label=u"Bains",
-                               css_def=u"background-color: #64FE2E; color: black;"),
-                          dict(cat_name=u"colorBureaux",
-                               label=u"Bureaux",
-                               css_def=u"background-color: orange; color: black;"),
-                          dict(cat_name=u"colorDressings",
-                               label=u"Dressings",
-                               css_def=u"background-color: aqua; color: black;"),
-                          dict(cat_name=u"colorMagasins",
-                               label=u"Magasins",
-                               css_def=u"background-color: pink; color: black;"),
-                          dict(cat_name=u"colorMeubles",
-                               label=u"Meubles",
-                               css_def=u"background-color: red; color: black;")]})
+                         [dict(cat_name=u"colorMagasin",
+                               label=u"Magasin",
+                               css_def=u"background-color: #ea750c; color: white;"),  # @IgnorePep8
+                          dict(cat_name=u"colorBureau",
+                               label=u"Bureau",
+                               css_def=u"background-color: #f6a9bc; color: black;"),  # @IgnorePep8
+                          dict(cat_name=u"colorDressing",
+                               label=u"Dressing",
+                               css_def=u"background-color: #37ab51; color: white;"),  # @IgnorePep8
+                          dict(cat_name=u"colorMeuble",
+                               label=u"Meuble",
+                               css_def=u"background-color: #bf0e1d; color: white;"),  # @IgnorePep8
+                          dict(cat_name=u"colorMenuiserie",
+                               label=u"Menuiserie",
+                               css_def=u"background-color: #f7c181; color: black;"),  # @IgnorePep8
+						  dict(cat_name=u"colorBain",
+                               label=u"Salle de bain",
+                               css_def=u"background-color: #009fe5; color: white;"),  # @IgnorePep8
+                          dict(cat_name=u"colorCuisine",
+                               label=u"Cuisine",
+                               css_def=u"background-color: #fcc51c; color: black;")]})  # @IgnorePep8
         cat_dict.update({u"Projets interne":
                          [dict(cat_name=u"colorRenovation",
                                label=u"Rénovation",
-                               css_def=u"background-color: #0B610B; color: white;")]})
+                               css_def=u"background-color: #0B610B; color: white;")]})  # @IgnorePep8
         cat_dict.update({u"Hors projet":
                          [dict(cat_name=u"colorConges",
                                 label=u"Congés",
-                                css_def=u"background-color: #6A0888; color: white;"),
+                                css_def=u"background-color: #6A0888; color: white;"),  # @IgnorePep8
                            dict(cat_name=u"colorNettoyage",
                                 label=u"Nettoyage",
-                                css_def=u"background-color: #61380B; color: white;"),
+                                css_def=u"background-color: #61380B; color: white;"),  # @IgnorePep8
                            dict(cat_name=u"colorDechargement",
                                 label=u"Déchargement",
-                                css_def=u"background-color: #8A2908; color: white;")]})
+                                css_def=u"background-color: #8A2908; color: white;")]})  # @IgnorePep8
         for group, entry_list in cat_dict.iteritems():
             LOG.info(u"Add order category's group: {}".format(group))
             for entry in entry_list:
