b332b8643aa0871b665ec49fc257dbb2b8c0f798
Laurent LAPORTE <tantale.solutions@gmail.com>
2015-12-19 15:36:23 +0100

    FIX: Restore missing tables in migration scripts. Update database.

--- Intranet-1.0/intranet/maintenance/versions/v02_00/mapping.py
+++ Intranet-1.0/intranet/maintenance/versions/v02_00/mapping.py
@@ -2,3 +2,30 @@
 """The application's model objects"""
 from __future__ import absolute_import
 
+# noinspection PyUnresolvedReferences
+from .pointage.employee import Employee as TargetEmployee
+# noinspection PyUnresolvedReferences
+from .pointage.order import Order as TargetOrder
+# noinspection PyUnresolvedReferences
+from .pointage.order_phase import OrderPhase as TargetOrderPhase
+# noinspection PyUnresolvedReferences
+from .pointage.order_cat import OrderCat as TargetOrderCat
+# noinspection PyUnresolvedReferences
+from .pointage.cal_event import CalEvent as TargetCalEvent
+
+# noinspection PyUnresolvedReferences
+from .planning.week_day import WeekDay as TargetWeekDay
+# noinspection PyUnresolvedReferences
+from .planning.week_hours import WeekHours as TargetWeekHours
+# noinspection PyUnresolvedReferences
+from .planning.day_period import DayPeriod as TargetDayPeriod
+# noinspection PyUnresolvedReferences
+from .planning.hours_interval import HoursInterval as TargetHoursInterval
+# noinspection PyUnresolvedReferences
+from .planning.frequency import Frequency as TargetFrequency
+# noinspection PyUnresolvedReferences
+from .planning.calendar import Calendar as TargetCalendar
+# noinspection PyUnresolvedReferences
+from .planning.year_period import YearPeriod as TargetYearPeriod
+# noinspection PyUnresolvedReferences
+from .planning.planning_event import PlanningEvent as TargetPlanningEvent
